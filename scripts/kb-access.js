740e177753675c591a63b97a44d41d8b8719c2c3
BasileDM <125025171+BasileDM@users.noreply.github.com>
2024-01-25 20:43:48 +0100

    Dark mode toggle with tab fixed

--- scripts/kb-access.js
+++ scripts/kb-access.js
@@ -10,11 +10,25 @@ function onKeyUp(event, buttonID){
     if (event.key === "Enter") {
         switch (buttonID) {
             case 1 :
-                apparitionBoite();
+                // Dark mode
+                darkModeToggle();
                 break
             case 2 :
-                darkMode();
-                darkMode2();
+                // Language flag
+                switchLanguage();
+                break
+            case 3 :
+                // Call to action top
+                break
+            case 4 :
+                // Call to action boutonRose hero
+                break
+            case 5 :
+                // Hero arrow scroll
+                scrollToMain();
+                break
+            case 6 :
+                apparitionBoite();
                 break
         }
     }
